28b6e39190863b0d42c92f386372d4be2df34eea
pedrobarroso01 <pedrobarrosocontato@gmail.com>
2024-07-15 14:49:41 -0300

    Formatando a mensagem final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -22,6 +22,7 @@ public class ContaTerminal {
 
 
 
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",nomeCliente,agencia,numero,saldo);
 
     }
 }
